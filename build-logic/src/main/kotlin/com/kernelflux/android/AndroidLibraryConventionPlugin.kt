ce528eedcaed3cb5fcf0e6c9c6246297c094b995
kernelflux <ktappdev2024@gmail.com>
2025-06-15 19:33:14 +0800

    update convention plugin

--- build-logic/src/main/kotlin/com/kernelflux/android/AndroidLibraryConventionPlugin.kt
+++ build-logic/src/main/kotlin/com/kernelflux/android/AndroidLibraryConventionPlugin.kt
@@ -1,9 +1,13 @@
 package com.kernelflux.android
 
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.JavaVersion
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
@@ -12,22 +16,70 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 class AndroidModuleConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        println("✅ TestPlugin applied to: ${project.name}")
+        project.plugins.all {
+            when (this) {
+                is AppPlugin -> {
+                    safeApplyPlugin(project, "org.jetbrains.kotlin.android")
+                    safeApplyPlugin(project, "maven-publish")
+                    val ext = project.extensions.getByType(ApplicationExtension::class.java)
+                    configureApp(ext)
+                }
+                is LibraryPlugin -> {
+                    safeApplyPlugin(project, "org.jetbrains.kotlin.android")
+                    safeApplyPlugin(project, "maven-publish")
+                    val ext = project.extensions.getByType(LibraryExtension::class.java)
+                    configureLibrary(ext)
+                }
+            }
+        }
 
-        project.plugins.apply("com.android.library")
-        project.plugins.apply("org.jetbrains.kotlin.android")
-        project.plugins.apply("maven-publish")
 
-        project.extensions.configure<LibraryExtension> {
+        // 配置默认依赖
+        val libs = project.extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
+        project.afterEvaluate {
+            project.dependencies {
+                add("implementation", libs.findLibrary("androidx.core.ktx").get())
+                add("implementation", libs.findLibrary("androidx.appcompat").get())
+                add("implementation", libs.findLibrary("material").get())
+
+
+                add("testImplementation", libs.findLibrary("junit").get())
+                add("androidTestImplementation", libs.findLibrary("androidx.junit").get())
+            }
+        }
+
+
+        // ✅ Kotlin compilerOptions 设置
+        project.plugins.withId("org.jetbrains.kotlin.android") {
+            project.extensions.configure<KotlinAndroidProjectExtension> {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_11)
+                }
+            }
+        }
+    }
+
+    private fun safeApplyPlugin(project: Project, pluginName: String) {
+        if (!project.plugins.hasPlugin(pluginName)) {
+            project.plugins.apply(pluginName)
+        }
+    }
+
+    private fun configureApp(extension: ApplicationExtension) {
+        extension.apply {
             compileSdk = 35
+
             defaultConfig {
+                applicationId = "com.kernelflux.uixkitsample"
                 minSdk = 21
+                targetSdk = 35
+                versionCode = 1
+                versionName = "1.0.0"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-                consumerProguardFiles("consumer-rules.pro")
             }
 
             buildTypes {
-                getByName("release") {
+                release {
                     isMinifyEnabled = false
                     proguardFiles(
                         getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -42,7 +94,39 @@ class AndroidModuleConventionPlugin : Plugin<Project> {
             }
 
             buildFeatures {
-                buildConfig = true
+                viewBinding = true
+            }
+        }
+    }
+
+
+    private fun configureLibrary(extension: LibraryExtension) {
+        extension.apply {
+            compileSdk = 35
+
+            defaultConfig {
+                minSdk = 21
+                testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                consumerProguardFiles("consumer-rules.pro")
+            }
+
+            buildTypes {
+                release {
+                    isMinifyEnabled = false
+                    proguardFiles(
+                        getDefaultProguardFile("proguard-android-optimize.txt"),
+                        "proguard-rules.pro"
+                    )
+                }
+            }
+
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+
+            buildFeatures {
+                viewBinding = true
             }
 
             publishing {
@@ -51,30 +135,6 @@ class AndroidModuleConventionPlugin : Plugin<Project> {
                 }
             }
         }
-
-        // ✅ 配置 Kotlin 编译参数（推荐方式）
-        project.extensions.configure<KotlinAndroidProjectExtension> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_11)
-            }
-        }
-
-        // 配置默认依赖
-        project.afterEvaluate {
-            project.dependencies {
-                // AndroidX 默认依赖，可选按需启用
-                add("implementation", "androidx.core:core-ktx:1.13.1")
-                add("implementation", "androidx.appcompat:appcompat:1.6.1")
-                add("implementation", "com.google.android.material:material:1.11.0")
-
-                // 测试依赖
-                add("testImplementation", "junit:junit:4.13.2")
-                add("androidTestImplementation", "androidx.test.ext:junit:1.1.5")
-                add("androidTestImplementation", "androidx.test.espresso:espresso-core:3.5.1")
-            }
-        }
-
     }
-
 
 }
